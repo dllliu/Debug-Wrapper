bd5326c3d4728f0e31bd78ced18c1a116bb73e9f
dllliu <dliu30@stuy.edu>
2022-08-01 15:28:23 -0400

    state debug

--- Assets/DeBug.cs
+++ Assets/DeBug.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class DeBug : MonoBehaviour
 {
-    /*
+    
     private bool debugInfo = false;
     private int wheelIndex = 0;
     private int pedalIndex = 1;
+    private int masterIndex = 2;
 
     private bool FWheel;
 
     private bool MasterSteeringWheel = false;
-    */
+    private float slaveSteering = 0;
+    private float steerInput = 0f;
+    
     
     // Start is called before the first frame update
+    
     void Start()
     {
         DirectInputWrapper.Init();
@@ -26,7 +30,26 @@ public class DeBug : MonoBehaviour
         Debug.Log("The amount of devices found: " + DirectInputWrapper.DevicesCount());
         for (int i = 0; i < DirectInputWrapper.DevicesCount(); i++)
         {
-            Debug.Log("The current " + i + " device is: " + DirectInputWrapper.GetProductName(i));
+            Debug.Log("The current " + i + " device is: " + DirectInputWrapper.GetProductNameManaged(i));
         }
+       
+        DeviceState state;
+        DeviceState slaveState;
+
+           
+        if (MasterSteeringWheel) {
+            state = DirectInputWrapper.GetStateManaged (masterIndex);
+            slaveState = DirectInputWrapper.GetStateManaged (wheelIndex);
+            slaveSteering = slaveState.lX / 32768f;
+        } else {
+               
+            state = DirectInputWrapper.GetStateManaged (wheelIndex);
+        }
+
+        steerInput = state.lX / 32768f;
+//accelInput = state.rglSlider [0] / -32768f;
+        Debug.Log(DirectInputWrapper.GetProductNameManaged(wheelIndex));
+
+        Debug.Log("Device One: \tlRx: " + state.lRx + "\tlRy: " + state.lRy + "\tlRz: " + state.lRz + "\tlX: " + state.lX + "\tlY: " + state.lY + "\tlZ: " + state.lZ);
     }
 }
