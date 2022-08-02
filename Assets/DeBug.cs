c732be67072cd2d88db465b53b91fd0b430c03e4
srinathsibi <sibisrinath@gmail.com>
2022-08-02 10:23:52 -0400

    rep error

--- Assets/DeBug.cs
+++ Assets/DeBug.cs
@@ -15,6 +15,7 @@ public class DeBug : MonoBehaviour
     private bool MasterSteeringWheel = false;
     private float slaveSteering = 0;
     private float steerInput = 0f;
+
     
     
     // Start is called before the first frame update
@@ -24,32 +25,30 @@ public class DeBug : MonoBehaviour
         DirectInputWrapper.Init();
     }
 
+
+
     // Update is called once per frame
     void Update()
     {
+        DirectInputWrapper.Update();
         Debug.Log("The amount of devices found: " + DirectInputWrapper.DevicesCount());
         for (int i = 0; i < DirectInputWrapper.DevicesCount(); i++)
         {
             Debug.Log("The current " + i + " device is: " + DirectInputWrapper.GetProductNameManaged(i));
+
+            try
+            {
+                DeviceState state;
+                state = DirectInputWrapper.GetStateManaged(i);
+                Debug.Log("Device:" + i.ToString() + " \tlRx: " + state.lRx + "\tlRy: " + state.lRy + "\tlRz: " + state.lRz + "\tlX: " + state.lX + "\tlY: " + state.lY + "\tlZ: " + state.lZ);
+
+            }
+            catch 
+            {
+                Debug.Log("somethn went rong !!!");
+                //throw;
+            }
+            
         }
-       
-        DeviceState state;
-        DeviceState slaveState;
-
-           
-        if (MasterSteeringWheel) {
-            state = DirectInputWrapper.GetStateManaged (masterIndex);
-            slaveState = DirectInputWrapper.GetStateManaged (wheelIndex);
-            slaveSteering = slaveState.lX / 32768f;
-        } else {
-               
-            state = DirectInputWrapper.GetStateManaged (wheelIndex);
-        }
-
-        steerInput = state.lX / 32768f;
-//accelInput = state.rglSlider [0] / -32768f;
-        Debug.Log(DirectInputWrapper.GetProductNameManaged(wheelIndex));
-
-        Debug.Log("Device One: \tlRx: " + state.lRx + "\tlRy: " + state.lRy + "\tlRz: " + state.lRz + "\tlX: " + state.lX + "\tlY: " + state.lY + "\tlZ: " + state.lZ);
     }
 }
